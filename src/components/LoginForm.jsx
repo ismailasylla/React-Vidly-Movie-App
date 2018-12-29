4ae1bd14df23199af5f2695e5b2787ab56744097
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2018-12-29 22:58:43 +0530

    implimenting the validation using joi library

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
 import Input from "./common/Input";
+import Joi from 'joi-browser'
 
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
     errors:{}
   };
-  validate = () =>{
-      const errors = {};
-      const { account }= this.state;
-      if(account.username.trim() === '')
-      errors.username = 'Username is required.';
-      if(account.password.trim() === '')
-      errors.password ='Password is required.';
 
-    return Object.keys(errors).length === 0 ? null : errors;
+  schema ={
+    username: Joi.string().required().label('Username'),
+    password: Joi.string().required().label('Password')
+
+  }
+
+
+  validate = () =>{
+    const options = { abortEarly: false }
+    const {error} = Joi.validate(this.state.account, this.schema, options);
+    if(!error) return null;
+
+    const errors = {};
+    for (let item of error.details)
+    errors[item.path[0]] = item.message;
+    return errors;
+
   };
 
   handleSubmit = e => {
