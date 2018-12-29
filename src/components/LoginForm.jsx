4225b045cf906137997cf1450f3a3e58b8985a7c
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2018-12-29 22:25:44 +0530

    validation on Change

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,39 +3,68 @@ import Input from "./common/Input";
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    errors:{}
   };
-  componentDidMount() {}
+  validate = () =>{
+      const errors = {};
+      const { account }= this.state;
+      if(account.username.trim() === '')
+      errors.username = 'Username is required.';
+      if(account.password.trim() === '')
+      errors.password ='Password is required.';
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = e => {
     e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if(errors) return;
     //call the server
   };
-  handlaChange = ({ currentTarget: input}) => {
+
+  validateProperty = ({name, value}) => {
+      if(name === 'username'){
+      if (value.trim() === '') return 'Username is required.'
+    }
+      if(name === 'password'){
+      if (value.trim() === '') return 'Password is required.'
+    }
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = {...this.state.errors}
+    const errorMessage  = this.validateProperty(input)
+    if(errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name]
+
     const account = { ...this.state.account };
     account[input.name] = input.value;
-    this.setState({ account });
+    this.setState({ account, errors });
   };
   render() {
-      const { account } = this.state;
+      const { account, errors } = this.state;
 
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
 
-         <Input 
-         name ="username"
-         value={account.username}
-         label="Username" 
-         onChange={this.handleChange}/>
-
-         <Input 
-         name ="password"
-         value={account.password}
-         label="Password" 
-         onChange={this.handleChange}/>
-
-         
+            <Input 
+            name ="username"
+            label="Username" 
+            value={account.username}
+            error= {errors.username}
+            onChange={this.handleChange}/>
+            <Input 
+            name ="password"
+            label="Password" 
+            value={account.password}
+            error={errors.password}
+            onChange={this.handleChange}/>
+            
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
-const Input = ({ name, label, value, onChange }) => {
-  return (
-    <div className="form-group">
-      <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        name={name}
-        type="text"
-        className="form-control"
-      />
-    </div>
-  );
+const Input = ({ name, label, value, error, onChange }) => {
+    return (
+        <div className="form-group">
+            <label htmlFor={name}>{label}</label>
+            <input
+                name={name}
+                value={value}
+                onChange={onChange}
+                id={name}
+                type="text"
+                className="form-control"
+            />
+            {error && <div className="alert alert-danger">{error}</div>}
+        </div>
+    );
 };
- 
+
 export default Input;
