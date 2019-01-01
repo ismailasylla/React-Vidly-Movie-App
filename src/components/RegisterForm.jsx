70a6c7de2ac94c3971b3a3c87b893f4c4f417809
Sylla Ismaila <syllaismaila@Syllas-MacBook-Pro.local>
2019-01-01 14:51:49 +0530

    Handling Registration Errors

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Form from "./common/Form";
 import Joi from "joi-browser";
+import * as userService from '../services/userService';
 
 class RegisterForm extends Form {
     state = {
@@ -22,9 +23,18 @@ class RegisterForm extends Form {
             .label('Name')    
     };
 
-    doSubmit = () => {
+    doSubmit = async ()  => {
         //call the server
-        console.log("Submitted!");
+        try{
+            await userService.register(this.state.data);
+        }
+        catch(ex){
+            if(ex.response && ex.response.status === 400){
+                const errors = {...this.state.errors};
+                errors.username = ex.response.data;
+                this.setState({errors});
+            }
+        }
     };
 
     render() {
---
+++ src/services/userService.js
@@ -0,0 +1,14 @@
+import http from "../services/httpService";
+import { apiUrl } from "../config.json";
+
+const apiEndpoint = apiUrl + "/users";
+
+
+export function register(user){
+   return http.post(apiEndpoint, {
+        email: user.username,
+        password: user.password,
+        name: user.name
+    });
+}
+ 
